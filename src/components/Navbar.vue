a27d4cd0c1c54b7a4af73665019c6d9d2ff72eef
Ggahramaq <gusikgahramanli2012@gmail.com>
2025-02-22 11:41:11 +0400

    Code the navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,3 +1,30 @@
+<script setup>
+import logo from '@/assets/shared/logo.svg';
+import { RouterLink } from 'vue-router';
+</script>
+
 <template>
-    Navbar
+    <div class="navbar flex w-screen justify-between items-center mt-5 absolute bg-none backdrop-blur-sm z-50">
+        <RouterLink to="/">
+        <img class="h-12 w-auto ml-15" :src="logo" alt="Space tourism">    
+        </RouterLink>
+        <div class="link flex items-center w-176 h-25 mr-37 justify-evenly text-white text-2xl">
+            <RouterLink to="/" class="transition duration-200 ease-out hover:text-green-400">
+                00
+                HOME
+            </RouterLink>
+            <RouterLink to="/destination" class="transition duration-200 ease-out hover:text-green-400">
+                01
+                DESTINATION
+            </RouterLink>
+            <RouterLink to="/crew" class="transition duration-200 ease-out hover:text-green-400">
+                02
+                CREW
+            </RouterLink>
+            <RouterLink to="/technology" class="transition duration-200 ease-out hover:text-green-400">
+                03
+                TECHNOLOGY
+            </RouterLink>
+        </div>
+    </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,10 @@
 <script setup>
 import { RouterLink } from 'vue-router';
+import background from '@/assets/img/home-bg.jpg'
 </script>
 
 <template>
-    Home page
+  <div class="relative w-screen h-screen">
+    <img :src="background" class="absolute top-0 left-0 w-full h-full object-cover" />
+  </div>
 </template>

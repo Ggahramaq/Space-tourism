873696311cd1afe394a4c692cef5b838d804f170
Ggahramaq <125120060+Ggahramaq@users.noreply.github.com>
2025-02-22 20:05:25 +0400

    Update DestinationView.vue

--- src/views/DestinationView.vue
+++ src/views/DestinationView.vue
@@ -1,7 +1,24 @@
 <script setup>
+import background from '@/assets/img/destination-bg.jpg';
+import moonimg from '@/assets/img/moon-image.png';
 import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-    Destination page
+  <div class="relative w-screen h-screen">
+    <img :src="background" class="absolute top-0 left-0 w-full h-full object-cover" />
+    <div class="mb-10 ml-40 absolute flex items-center text-white text-3xl h-105 w-277 justify-between">
+        <div class="left-side">
+            <div class="flex mb-15">
+                <div class="number font-bold text-zinc-500">                
+                01
+                </div>
+                <div class="destination-heading ml-5">
+                    PICK YOUR DESTINATION
+                </div>
+            </div>
+            <img :src="moonimg" alt="Space" class="absolute w-110 h-110 ml-3">
+        </div>
+    </div>
+  </div>
 </template>

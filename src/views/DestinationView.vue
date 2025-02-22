5729d9d59b80f14901d90b9bf2e2cb056108316b
Ggahramaq <125120060+Ggahramaq@users.noreply.github.com>
2025-02-22 21:14:36 +0400

    DestinationView layout

--- src/views/DestinationView.vue
+++ src/views/DestinationView.vue
@@ -19,6 +19,43 @@ import { RouterLink } from 'vue-router';
             </div>
             <img :src="moonimg" alt="Space" class="absolute w-110 h-110 ml-3">
         </div>
+        <div class="right-side mt-90">
+            <div class="buttons cursor-pointer">
+                MOON
+                MARS
+                EUROPA
+                TITAN
+            </div>
+            <br>
+            <div class="right-side-heading">
+                MOON
+            </div>
+            <br>
+            <!-- <div class="right-side-description">
+                See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.
+            </div>
+            <br> -->
+            <div class="line"></div>
+            <br>   
+            <div class="flex justify-between">
+                <div class="distance">
+                    <div class="d-heading">
+                        AVG. DISTANCE
+                    </div>
+                    <div class="d-data">
+                        384,400 KM
+                    </div>
+                </div>
+                <div class="travel-time">
+                    <div class="d-heading">
+                        EST. TRAVEL TIME
+                    </div>
+                    <div class="d-data">
+                        3 DAYS
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   </div>
 </template>

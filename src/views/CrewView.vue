013358c4f95bd62acdf29cb4a39ab29b2ebc86df
Ggahramaq <125120060+Ggahramaq@users.noreply.github.com>
2025-02-23 12:06:46 +0400

    Add dynamic description

--- src/views/CrewView.vue
+++ src/views/CrewView.vue
@@ -49,6 +49,19 @@ const dynamicName = () => {
             return 'ANOUSHEH ANSARI'
     };
 }
+
+const dynamicDescription = () => {
+    switch (img.value) {
+        case hurley:
+            return 'Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.';
+        case mark:
+            return 'Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.';
+        case victor:
+            return 'Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer. ';
+        case anousheh:
+            return 'Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space. '
+    };
+}
 </script>
 
 <template>
@@ -71,7 +84,7 @@ const dynamicName = () => {
                     {{dynamicName()}}
                 </div>
                 <div class="rdescription mt-2 max-w-145 text-blue-200 text-[18px]">
-                    Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.
+                    {{dynamicDescription()}}
                 </div>
                 <br>
                 <div class="flex mt-10">

30ba043b679092d3fcca8c0ba871e1b55ca64acc
Ggahramaq <125120060+Ggahramaq@users.noreply.github.com>
2025-02-23 16:00:05 +0400

    I messed up the files

--- src/views/CrewView.vue
+++ src/views/CrewView.vue
@@ -1,37 +1,71 @@
 <script setup>
-import background from '@/assets/img/technology-bg.jpg';
-import lv from '@/assets/img/launchvehicle.jpg';
-import sc from '@/assets/img/spacecapsule.jpg';
-import sp from '@/assets/img/spaceport.jpg';
+import background from '@/assets/img/crew-bg.jpg'
+import hurley from '@/assets/img/hurley.png'
+import mark from '@/assets/img/mark.png';
+import victor from '@/assets/img/victor.png';
+import anousheh from '@/assets/img/anousheh.png';
 import { ref } from 'vue';
 
-const img = ref(lv);
-const imgstring = ref('LAUNCH VEHICLE')
+const img = ref(hurley);
+const imgstring = ref('hurley');
 
-const lvBtn = () => {
-    img.value = lv
-    imgstring.value = 'LAUNCH VEHICLE'
+const hurleyBtn = () => {
+    img.value = hurley
+    imgstring.value = 'hurley'
 }
 
-const scBtn = () => {
-    img.value = sc
-    imgstring.value = 'SPACE CAPSULE'
+const markBtn = () => {
+    img.value = mark
+    imgstring.value = 'mark'
 }
 
-const spBtn = () => {
-    img.value = sp
-    imgstring.value = 'SPACEPORT'
+const victorBtn = () => {
+    img.value = victor
+    imgstring.value = 'victor'
 }
 
-const showDescription = (name) => {
-    switch (name) {
-        case 'LAUNCH VEHICLE':
-            return `A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!`;
-        case 'SPACEPORT':
-            return `A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.`;
-        case 'SPACE CAPSULE':
-            return `A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.`;
-    }
+const anoushehBtn = () => {
+    img.value = anousheh
+    imgstring.value = 'anousheh'
+}
+
+const dynamicRole = () => {
+    switch (img.value) {
+        case hurley:
+            return 'COMMANDER';
+        case mark:
+            return 'MISSION SPECIALIST';
+        case victor:
+            return 'PILOT';
+        case anousheh:
+            return 'FLIGHT ENGINEER'
+    };
+}
+
+const dynamicName = () => {
+    switch (img.value) {
+        case hurley:
+            return 'DOUGLAS HURLEY';
+        case mark:
+            return 'MARK SHUTTLEWORTH';
+        case victor:
+            return 'VICTOR GLOVER';
+        case anousheh:
+            return 'ANOUSHEH ANSARI'
+    };
+}
+
+const dynamicDescription = () => {
+    switch (img.value) {
+        case hurley:
+            return 'Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.';
+        case mark:
+            return 'Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.';
+        case victor:
+            return 'Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer. ';
+        case anousheh:
+            return 'Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space. '
+    };
 }
 
 const isActive = (name) => {
@@ -44,40 +78,33 @@ const isActive = (name) => {
     <img :src="background" class="absolute top-0 left-0 w-full h-full object-cover" />
         <div class="mb-10 ml-40 absolute flex items-center text-white text-3xl h-105 w-277 justify-between">
             <div class="left-side">
-                <div class="flex ml-4 mt-75">
+                <div class="flex mt-105">
                     <div class="number font-bold text-zinc-500">                
-                        03
+                        02
                     </div>
                     <div class="destination-heading ml-5">
-                        SPACE LAUNCH 101
+                        MEET YOUR CREW
                     </div>
                 </div>
-                <div class="flex mt-30">
-                    <div class="left">
-                        <div :class="[isActive('LAUNCH VEHICLE') ? 'bg-white text-black hover:bg-transparent hover:text-blue-400' : 'hover:bg-blue-400 hover:text-black border-white text-white bg-transparent', 'transition', 'duration-200', 'ease-out', 'h-15', 'w-15', 'aspect-square', 'border', 'rounded-full', 'cursor-pointer']" @click="lvBtn"><div class="mt-3 ml-6">
-                            1
-                        </div>
-                    </div>
-                        <br>
-                        <div :class="[isActive('SPACEPORT') ? 'bg-white text-black hover:bg-transparent hover:text-blue-400' : 'hover:bg-white hover:text-black border-white text-white bg-none', 'transition', 'duration-200', 'ease-out', 'h-15', 'w-15', 'aspect-square', 'border', 'rounded-full', 'cursor-pointer']" @click="spBtn"><div class="mt-3 ml-5">
-                            2
-                        </div>
-                    </div>
-                        <br>
-                        <div :class="[isActive('SPACE CAPSULE') ? 'bg-white text-black hover:bg-transparent hover:text-blue-400' : 'hover:bg-white hover:text-black border-white text-white bg-none', 'transition', 'duration-200', 'ease-out', 'h-15', 'w-15', 'aspect-square', 'border', 'rounded-full', 'cursor-pointer']" @click="scBtn"><div class="mt-3 ml-5">
-                            3
-                        </div>
-                    </div>
-                    </div>
-                    <div class="right ml-15">
-                        <div class="t-main mt-[-10px] text-[40px] text-zinc-400 absolute">THE TERMINOLOGY...</div>
-                        <div class="t-head mt-11 text-[65px] absolute">{{imgstring}}</div>
-                        <div class="t-description mt-31 text-[23px] text-blue-200 absolute max-w-160">{{showDescription(imgstring)}}</div>
-                    </div>
+                <div class="role text-zinc-400 mt-30">
+                    {{dynamicRole()}}
+                </div>
+                <div class="role text-[60px] mt-2">
+                    {{dynamicName()}}
+                </div>
+                <div class="rdescription mt-2 max-w-145 text-blue-200 text-[18px]">
+                    {{dynamicDescription()}}
+                </div>
+                <br>
+                <div class="flex mt-10">
+                    <button :class="[isActive('hurley') ? 'bg-zinc-600 hover:bg-zinc-800' : 'bg-zinc-400 hover:bg-zinc-600', 'transition', 'duration-200', 'ease-out', 'border', 'rounded-full', 'mr-12', 'cursor-pointer', 'aspect-square', 'h-[20px]', 'w-[20px]']" @click="hurleyBtn"></button>
+                    <button :class="[isActive('mark') ? 'bg-zinc-600 hover:bg-zinc-800' : 'bg-zinc-400 hover:bg-zinc-600', 'transition', 'duration-200', 'ease-out', 'border', 'rounded-full', 'mr-12', 'cursor-pointer', 'aspect-square', 'h-[20px]', 'w-[20px]']" @click="markBtn"></button>
+                    <button :class="[isActive('victor') ? 'bg-zinc-600 hover:bg-zinc-800' : 'bg-zinc-400 hover:bg-zinc-600', 'transition', 'duration-200', 'ease-out', 'border', 'rounded-full', 'mr-12', 'cursor-pointer', 'aspect-square', 'h-[20px]', 'w-[20px]']" @click="victorBtn"></button>
+                    <button :class="[isActive('anousheh') ? 'bg-zinc-600 hover:bg-zinc-800' : 'bg-zinc-400 hover:bg-zinc-600', 'transition', 'duration-200', 'ease-out', 'border', 'rounded-full', 'mr-12', 'cursor-pointer', 'aspect-square', 'h-[20px]', 'w-[20px]']" @click="anoushehBtn"></button>
                 </div>
             </div>
-        <div class="right-side">
-            <img :src="img" alt="asd" class="h-120 mt-110 ml-120 border-0 rounded-lg">
+        <div class="right-side mt-111">
+            <img :src="img" :alt="dynamicName()" class="h-155">
         </div>
     </div>
   </div>

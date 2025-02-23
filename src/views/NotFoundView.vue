a87d57c4a1b371b5732d60a630025e385ed9891d
Ggahramaq <125120060+Ggahramaq@users.noreply.github.com>
2025-02-23 19:30:57 +0400

    Update NotFoundView.vue

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,7 +1,13 @@
 <script setup>
+import background from '@/assets/img/destination-bg.jpg';
 import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-  Page not found! :(
+  <img :src="background" class="absolute top-0 left-0 w-full h-full object-cover">
+  <div class="mb-10 ml-40 absolute flex items-center text-white text-3xl h-105 w-277 justify-center">
+    <div class="destination-heading ml-5">
+                    PAGE NOT FOUND :(
+      </div>
+  </div>
 </template>

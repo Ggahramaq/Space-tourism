6d511b25111bb1a4b739fd1cd496de4de51c6b87
Ggahramaq <125120060+Ggahramaq@users.noreply.github.com>
2025-02-23 14:59:49 +0400

    Add styles

--- src/views/TechnologyView.vue
+++ src/views/TechnologyView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import background from '@/assets/img/technology-bg.jpg'
+import img from '@/assets/img/launchvehicle.jpg'
 import { RouterLink } from 'vue-router';
 
 </script>
@@ -9,7 +10,7 @@ import { RouterLink } from 'vue-router';
     <img :src="background" class="absolute top-0 left-0 w-full h-full object-cover" />
         <div class="mb-10 ml-40 absolute flex items-center text-white text-3xl h-105 w-277 justify-between">
             <div class="left-side">
-                <div class="flex mt-90">
+                <div class="flex ml-4 mt-95">
                     <div class="number font-bold text-zinc-500">                
                         03
                     </div>
@@ -17,8 +18,8 @@ import { RouterLink } from 'vue-router';
                         SPACE LAUNCH 101
                     </div>
                 </div>
-                <div class="flex mt-35">
-                    <div class="left">
+                <div class="flex mt-20">
+                    <div class="left mt-10">
                         <div class="h-15 w-15 aspect-square border rounded-full bg-none border-white text-white"><div class="mt-3 ml-6">
                             1
                         </div>
@@ -34,15 +35,15 @@ import { RouterLink } from 'vue-router';
                         </div>
                     </div>
                     </div>
-                    <div class="right ml-3">
-                        <div class="t-main">THE TERMINOLOGY...</div>
-                        <div class="t-head">LAUNCH VEHICLE</div>
-                        <div class="t-description">A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!</div>
+                    <div class="right ml-15">
+                        <div class="t-main text-[40px] text-zinc-400">THE TERMINOLOGY...</div>
+                        <div class="t-head mt-5 text-[65px]">LAUNCH VEHICLE</div>
+                        <div class="t-description mt-3 text-[27px] text-blue-200 max-w-218">A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!</div>
                     </div>
                 </div>
             </div>
-        <div class="right-side mt-111">
-            <!-- <img :src="img" :alt="dynamicName()" class="h-155"> -->
+        <div class="right-side">
+            <img :src="img" alt="asd" class="h-120 mt-105 ml-42">
         </div>
     </div>
   </div>
